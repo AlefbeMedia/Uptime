f2062c0543ee4c85eb8fec88c1c4f64c8d05a8d8
alefbemedia <110613191+AlefbeMedia@users.noreply.github.com>
2024-11-13 21:52:29 +0330

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,3 +6,5 @@ interface=$(ip route | grep default | awk '{print $5}')
 resolvectl dns $interface 178.22.122.100 185.51.200.2
 echo -e "nameserver 178.22.122.100\nnameserver 185.51.200.2" > /etc/resolv.conf
 docker login -u $username
+resolvectl dns $interface 8.8.8.8 8.8.4.4
+echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" > /etc/resolv.conf
