cfaafea1d54558ab626e61a72bfeeb82a87af4c0
alefbemedia <110613191+AlefbeMedia@users.noreply.github.com>
2024-11-13 21:46:43 +0330

    Update setdns.sh

--- setdns.sh
+++ setdns.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 clear
 
-read -p "Enter your Docker username: " username
+#read -p "Enter your Docker username: " username
 interface=$(ip route | grep default | awk '{print $5}')
 resolvectl dns $interface 178.22.122.100 185.51.200.2
 echo -e "nameserver 178.22.122.100\nnameserver 185.51.200.2" > /etc/resolv.conf
-docker login -u $username
+docker login
