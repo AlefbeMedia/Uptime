c9b8937109703655e921079f1228b170225fdb4d
alefbemedia <110613191+AlefbeMedia@users.noreply.github.com>
2024-11-13 21:31:44 +0330

    Update setdns.sh

--- setdns.sh
+++ setdns.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 clear
 
+read -p "Enter your Docker username: " username
 interface=$(ip route | grep default | awk '{print $5}')
 resolvectl dns $interface 178.22.122.100 185.51.200.2
-cp /etc/resolv.conf /etc/resolv.conf.backup
 echo -e "nameserver 178.22.122.100\nnameserver 185.51.200.2" > /etc/resolv.conf
-docker login
-resolvectl dns $interface 8.8.8.8 8.8.4.4
-echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" > /etc/resolv.conf
+docker login -u $username
